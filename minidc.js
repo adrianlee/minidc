84b8a40405f42750206ea6256090b604e09a90a2
Adrian Lee <jun.irok@gmail.com>
2013-03-01 19:02:55 -0500

    Full implementation. Missing test cases

--- minidc.js
+++ minidc.js
@@ -1,31 +1,88 @@
-// var program = require('commander');
-
-// program
-//   .version('0.0.1');
-
-// program.parse(process.argv);
-
-
-// function getInput() {
-//   program.prompt('', function (input) {
-//     console.log('%s', input);
-//     getInput();
-//   });
-// }
-
-// getInput();
-
-
 var readline = require('readline');
+var _ = require('lodash');
 
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-rl.question("What do you think of node.js? ", function(answer) {
-  // TODO: Log the answer in a database
-  console.log("Thank you for your valuable feedback:", answer);
+module.exports = rl;
 
-  rl.close();
+rl.setPrompt('dc > ');
+rl.prompt();
+
+var stack = [];
+
+rl.on('line', function(line) {
+  switch(line.trim()) {
+    // prints value of top element on stack. no alternation
+    case 'p':
+      if (stack[stack.length-1] == "undefined") { // no minux -1 if stack.length 0
+        return console.log("stack empty")
+      }
+      console.log(stack[stack.length-1]);
+      break;
+    // pops top element and prints
+    case 'n':
+      if (stack[stack.length-1] == "undefined") {
+        return console.log("stack empty")
+      }
+      console.log(stack.pop());
+      break;
+    // prints all in reverse order. no alteration
+    case 'f':
+      for (var i = stack.length-1; i >= 0; i--) {
+        console.log(stack[i]);
+      }
+      break;
+    // pops two values and adds them. pushes result into stack
+    case '+':
+      var i = parseInt(stack.pop()) + parseInt(stack.pop());
+      stack.push(i);
+      break;
+
+    // pops two values and subtracts them. pushes result into stack
+    case '-':
+      var i =parseInt(stack.pop());
+      var j =parseInt(stack.pop());
+      var res = j -i;
+      stack.push(res);
+      break;
+
+    // pops two values and multiplies them. pushes result into stack
+    case '*':
+      var i = parseInt(stack.pop()) * parseInt(stack.pop());
+      stack.push(i);
+      break;
+
+    // pops two values and multiplies them. pushes result into stack
+    case '*':
+      var i = parseInt(stack.pop()) * parseInt(stack.pop());
+      stack.push(i);
+      break;
+
+    // pops two values, divides 2nd from 1st multiplies them. pushes result into stack
+    case '/':
+      var i =parseInt(stack.pop());
+      var j =parseInt(stack.pop());
+      var res = j / i;
+      stack.push(res);
+      break;
+
+    case 'exit':
+      process.exit(0);
+      break;
+
+    default:
+      if (!isNaN(parseInt(line.trim()))) {
+        stack.push(line.trim())
+      } else {
+        console.log("unimplemented");
+      }
+      break;
+  }
+  rl.prompt();
+}).on('close', function() {
+  console.log('Have a great day!');
+  process.exit(0);
 });
